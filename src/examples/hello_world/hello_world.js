73b448fa99072b6e07cefa21aab3a66cc0d793e2
peter@biltcard.com <peter@biltcard.com>
2023-09-13 10:49:23 -0400

    simplifier hello world component

--- src/examples/hello_world/hello_world.js
+++ src/examples/hello_world/hello_world.js
@@ -2,23 +2,17 @@ looker.plugins.visualizations.add({
   // Id and Label are legacy properties that no longer have any function besides documenting
   // what the visualization used to have. The properties are now set via the manifest
   // form within the admin/visualizations page of Looker
-  id: "hello_world",
-  label: "Hello World",
   options: {
     font_size: {
       type: "string",
       label: "Font Size",
-      values: [
-        {"Large": "large"},
-        {"Small": "small"}
-      ],
+      values: [{ Large: "large" }, { Small: "small" }],
       display: "radio",
-      default: "large"
-    }
+      default: "large",
+    },
   },
   // Set up the initial state of the visualization
-  create: function(element, config) {
-
+  create: function (element, config) {
     // Insert a <style> tag with some styles we'll use later.
     element.innerHTML = `
       <style>
@@ -45,35 +39,18 @@ looker.plugins.visualizations.add({
 
     // Create an element to contain the text.
     this._textElement = container.appendChild(document.createElement("div"));
-
   },
   // Render in response to the data or settings changing
-  updateAsync: function(data, element, config, queryResponse, details, done) {
-
-    // Clear any errors from previous updates
+  updateAsync: function (data, element, config, queryResponse, details, done) {
     this.clearErrors();
 
-    // Throw some errors and exit if the shape of the data isn't what this chart needs
-    if (queryResponse.fields.dimensions.length == 0) {
-      this.addError({title: "No Dimensions", message: "This chart requires dimensions."});
-      return;
-    }
-
-    // Grab the first cell of the data
-    var firstRow = data[0];
-    var firstCell = firstRow[queryResponse.fields.dimensions[0].name];
-
-    // Insert the data into the page
-    this._textElement.innerHTML = LookerCharts.Utils.htmlForCell(firstCell);
-
-    // Set the size to the user-selected size
-    if (config.font_size == "small") {
-      this._textElement.className = "hello-world-text-small";
-    } else {
-      this._textElement.className = "hello-world-text-large";
-    }
+    console.log("data", data);
+    console.log("data", element);
+    console.log("config", config);
+    console.log("queryResponse", queryResponse);
+    console.log("queryResponse", details);
 
     // We are done rendering! Let Looker know.
-    done()
-  }
+    done();
+  },
 });
